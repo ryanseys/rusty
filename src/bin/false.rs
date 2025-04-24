08b53b9547a054b21c3a18264d36f3b9267d8fa3
Ryan Seys <ryan.seys@shopify.com>
2025-04-24 14:33:56 -0700

    add true and false binaries

---
+++ src/bin/false.rs
@@ -0,0 +1,4 @@
+// This is a simple program that returns a failure exit code.
+fn main() {
+    std::process::exit(1);
+}
---
+++ src/bin/true.rs
@@ -0,0 +1,4 @@
+// This is a simple program that returns a success exit code.
+fn main() {
+    std::process::exit(0);
+}
